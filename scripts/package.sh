2f8822d43eb8a01ab49eadc2ef56ec4850ddd5d2
Batfoxkid <batfoxkid@gmail.com>
2020-12-10 13:12:38 -0800

    Update Workflow

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,6 +1,10 @@
 cd build
 
+mkdir -p package/cfg
 mkdir -p package/addons/sourcemod/plugins
 
 cp -r addons/sourcemod/plugins/cclassrush.smx package/addons/sourcemod/plugins
 cp -r addons/sourcemod/plugins/cdeathrun.smx package/addons/sourcemod/plugins
+cp -r addons/sourcemod/plugins/ctimesten.smx package/addons/sourcemod/plugins
+cp -r ../cfg/randomizer_x10.cfg package/cfg
+cp -r ../LICENSE.txt package
